f907b3a700e59df6afde3ae85f25c66c9f4c8f79
Heino Tiia <a1704464@myy.haaga-helia.fi>
2018-09-09 16:35:20 +0300

    First commit

---
+++ bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
@@ -0,0 +1,15 @@
+package hh.swd20.bookstore.webcontrol;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String frontPage(Model model) {
+		return "index";
+	}
+}
