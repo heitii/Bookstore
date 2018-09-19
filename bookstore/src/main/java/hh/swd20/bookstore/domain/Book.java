29e2c80f24015ee5a493ba9edf997b1b2b201627
Heino Tiia <a1704464@myy.haaga-helia.fi>
2018-09-19 19:04:47 +0300

    added Crud, listpage and database

--- bookstore/src/main/java/hh/swd20/bookstore/domain/Book.java
+++ bookstore/src/main/java/hh/swd20/bookstore/domain/Book.java
@@ -1,7 +1,15 @@
 package hh.swd20.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
-	
+	@Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
 	private String title;
 	private String author;
 	private int year;
@@ -9,12 +17,19 @@ public class Book {
 	private double price;
 	
 	public Book(String title, String author, int year, String isbn, double price) {
-		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public Book() {
--- bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
@@ -1,15 +1,53 @@
 package hh.swd20.bookstore.webcontrol;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.bookstore.domain.Book;
+import hh.swd20.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookRepository bookRepository;
 
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public String frontPage(Model model) {
+	@RequestMapping(value="/index")
+	public String index() {
 		return "index";
+	}
+	
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+	public String bookList(Model model) {
+		model.addAttribute("books", bookRepository.findAll());
+		return "booklist";
+	}
+	
+	@RequestMapping(value="/addbook")
+	  public String addBook(Model model){
+	    model.addAttribute("book", new Book());
+	       return "addbook";
+	  }     
+	    
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	  public String save(Book book){
+		bookRepository.save(book);
+	    	return "redirect:booklist";
+	  }
+	
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
+	
+	@RequestMapping(value="/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		return "editbook";
 	}
 }
