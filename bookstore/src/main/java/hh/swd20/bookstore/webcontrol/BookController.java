4af38cc9f1f6ee3f5187abc6463d153674e28e61
Heino Tiia <a1704464@myy.haaga-helia.fi>
2018-09-28 12:28:12 +0300

    REST added

--- bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
@@ -1,11 +1,15 @@
 package hh.swd20.bookstore.webcontrol;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
@@ -30,6 +34,18 @@ public class BookController {
 	public String bookList(Model model) {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
+	}
+	
+	/** RESTful palvelu kaikkien kirjojen hakuun **/
+	@RequestMapping(value="/books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	/** RESTful palvelu kirjan hakuun id:n perusteella  **/
+	@RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return bookRepository.findById(bookId);
 	}
 	
 	/** Palauttaa tyhjän lomakkeen uuden kirjan lisäämiseen **/
