96a0f1b2fbbadb919c030455acc69ba0f18c86c7
Heino Tiia <a1704464@myy.haaga-helia.fi>
2018-10-05 16:56:40 +0300

    User, UserRepository and other added

--- bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/hh/swd20/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import hh.swd20.bookstore.domain.User;
+import hh.swd20.bookstore.domain.UserRepository;
 import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 import hh.swd20.bookstore.domain.Category;
@@ -21,12 +23,18 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository categoryrepository) {
+	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository categoryrepository, UserRepository userrepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			categoryrepository.save(new Category("Fantasy"));
 			categoryrepository.save(new Category("Historical"));
 			categoryrepository.save(new Category("Horror"));
+			
+			// Lisätään pari käyttäjää
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "user@email.fi", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "admin@email.fi", "ADMIN");
+			userrepository.save(user1);
+			userrepository.save(user2);
 			
 			bookrepository.save(new Book("Harry Potter and the Philosophers Stone", "J.K. Rowling", 1997, "0-7475-3269-9", 24.95, categoryrepository.findByName("Fantasy").get(0)));
 			bookrepository.save(new Book("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 1998, "0-7475-3849-2", 24.95, categoryrepository.findByName("Fantasy").get(0)));
--- bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
+++ bookstore/src/main/java/hh/swd20/bookstore/webcontrol/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,12 @@ public class BookController {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	/** Kirjautuu sisään **/
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
 
 	@RequestMapping(value="/index")
 	public String index() {
